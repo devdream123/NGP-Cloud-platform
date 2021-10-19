d81423758e0d389f0d757516c5443c2a829b0503
Tohid Azimi <TAzimi@quantium.com>
2021-10-19 11:15:36 +1100

    introduced the delivery pipeline

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+
+if [ ! "$1" ]; then
+	echo "usage: $0  <tenant (e.g. dev, uat, prd, canary, preprd)>"
+	exit 1
+fi
+
+
+environment=$1
+
+KUBECTL_DRY_RUN="${KUBECTL_DRY_RUN:-server}"
+
+
+echo ================================================================================
+echo "Deploying environment specific settings for $environment"
+echo ================================================================================
+kubectl apply --dry-run=$KUBECTL_DRY_RUN -R -f environments/deplyoments/$environment
+kubectl apply --dry-run=$KUBECTL_DRY_RUN -R -f environments/services/$environment
+
