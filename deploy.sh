f5c7860a9d338c9c302028eb3983c68b01b1dfeb
Tohid Azimi <TAzimi@quantium.com>
2021-11-16 14:52:38 +1100

    change cluster config

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,9 @@ KUBECTL_DRY_RUN="${KUBECTL_DRY_RUN:-server}"
 echo ================================================================================
 echo "Deploying environment specific settings for $environment"
 echo ================================================================================
+gcloud container clusters get-credentials qr-dev-analytics-backend --region us-central1 --project gcp-wow-corp-qretail-ngp-dev
+kubectl cluster-info
+echo $CLOUDSDK_CONTAINER_CLUSTER
 kubectl apply --dry-run=$KUBECTL_DRY_RUN -R -f environments/$environment/namespaces/
 kubectl apply --dry-run=$KUBECTL_DRY_RUN -R -f environments/$environment/deplyoments/
 kubectl apply --dry-run=$KUBECTL_DRY_RUN -R -f environments/$environment/services/
