ce2057be83303fb0a3fa4acd0eb19e1881cc6514
Tohid <91237829+tohid-Q@users.noreply.github.com>
2021-11-30 10:09:53 +1100

    Integrated Istio & healthcheck charts (#4)
    * Integrated Istio & healthcheck charts
    
    * refactoring to fix namespace creation and labelling
    
    * refactoring to fix namespace creation and labelling
    
    * git add changed path
    
    Co-authored-by: Dan Eyles <deyles@quantium.com.au>

--- scripts/build.sh
+++ scripts/build.sh
@@ -38,14 +38,23 @@ if [[ -z "${release_name}" || -z "${release_env}" ]]; then
 	exit 1
 fi
 
-mkdir -p "${base_dir}/charts-rendered/dev"
+rendered_chart_path="${base_dir}/../charts-rendered/dev"
+mkdir -p "${rendered_chart_path}"
+chart_path="${base_dir}/../charts"
 
-helm template "${base_dir}/charts/graphql-mesh" \
-	-f "${base_dir}/charts/graphql-mesh/values-dev.yaml" \
-	--atomic \
-	--name-template="${release_name}" > "${base_dir}/charts-rendered/dev/graphql-mesh-0.0.1.yml"
 
-helm template "${base_dir}/charts/dealsheet-api" \
-	-f "${base_dir}/charts/dealsheet-api/values-dev.yaml" \
+helm template "${chart_path}/istio" \
+  -f "${chart_path}/istio/values-dev.yaml" \
+  --atomic \
+  --name-template="${release_name}" > "${rendered_chart_path}/istio-0.0.1.yml"
+
+helm template "${chart_path}/graphql-mesh" \
+	-f "${chart_path}/graphql-mesh/values-dev.yaml" \
 	--atomic \
-	--name-template="${release_name}" > "${base_dir}/charts-rendered/dev/dealsheet-api-0.0.1.yml"
+	--name-template="${release_name}" > "${rendered_chart_path}/graphql-mesh-0.0.1.yml"
+
+helm template "${chart_path}/dealsheet-api" \
+	-f "${chart_path}/dealsheet-api/values-dev.yaml" \
+	--atomic \
+	--name-template="${release_name}" > "${rendered_chart_path}/dealsheet-api-0.0.1.yml"
+
