0c7e22108950609edb539873b4d751863e633f56
Tohid <91237829+tohid-Q@users.noreply.github.com>
2023-01-06 14:28:45 +1100

    Feature/nsps2 5284 package store helm charts (#612)
    * fix: NSPS2-xxx added env name for feature flags
    
    * NSPS2-5284 added pipeline for storing helm charts
    
    * added option
    
    * fixed syntax

--- scripts/package-store-charts.sh
+++ scripts/package-store-charts.sh
@@ -3,23 +3,28 @@
 set -e
 
 function print_usage() {
-  printf "a script to package and store helm charts on a remote repository.\n"
-  
+  printf "A script to package and store helm charts on a remote repository.\n"
+  printf "Artifact registry uri is required."
 }
 
+if [ ! "$1" ]; then
+	print_usage
+	exit 1
+fi
+
 script_dir=$(dirname "$0")
-root_dir=$(cd "${script_dir}/.."; pwd -P)
+export BASE_DIR=$(cd "${script_dir}"; pwd -P)
 
+charts_dir="${BASE_DIR}/../charts"
+destination_dir="${BASE_DIR}/../outputs/packages"
+artifact_registry_uri=$1
 
-charts_dir="${root_dir}/charts"
-destination_dir="${root_dir}/outputs/packages"
 
 for dir in  $(ls ${charts_dir}); do
    helm package "${charts_dir}/${dir}" -d ${destination_dir}
 done 
 
-
 for package in $(ls ${destination_dir}); do
-echo "${destination_dir}/${package}"
- helm push "${destination_dir}/${package}" oci://us-central1-docker.pkg.dev/gcp-wow-corp-infra-qrtl-prod/ngp-helm-charts
+ echo "${destination_dir}/${package}"
+ helm push "${destination_dir}/${package}" oci://${artifact_registry_uri}
 done
