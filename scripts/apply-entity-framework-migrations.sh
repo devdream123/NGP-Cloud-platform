2b7504c94c91c0da69eb725b135cf515f82331b8
Tohid <91237829+tohid-Q@users.noreply.github.com>
2023-03-31 14:47:22 +1100

    post deployment EF migration changes and multiple database connection string  (#952)
    * NSPS2-6644 added placeholder connection string
    
    * fixed the connection
    
    * cleaned-up unused variables
    
    * refacotored and enabled users in all env
    
    * fixed the syntax error

--- scripts/apply-entity-framework-migrations.sh
+++ scripts/apply-entity-framework-migrations.sh
@@ -9,9 +9,12 @@ function print_usage() {
   printf "Chart name is required.\n"
   printf "Cluster name is required.\n"
   printf "GCP project id is required.\n"
+  printf "Cloud sql instance name is required.\n"
+  printf "Database name is required.\n"
+  printf "Database IAM user is required.\n"
 }
 
-if [ ! "$1" ] || [ ! "$2" ] || [ ! "$3" ] || [ ! "$4" ]; then
+if [ ! "$1" ] || [ ! "$2" ] || [ ! "$3" ] || [ ! "$4" ] || [ ! "$5" ] || [[ ! "$6" ]] || [[ ! "$7" ]]; then
   print_usage
   exit 1
 fi
@@ -20,11 +23,11 @@ ef_migrations_bucket=$1
 chart_name=$2
 cluster_name=$3
 gcp_project=$4
+cloud_sql_instance=$5
+database=$6
+database_iam_user=$7
 
 tag=$(yq eval '.image.tag' "${BASE_DIR}/../charts/${chart_name}/values-${cluster_name}.yaml")
-cloud_sql_instance=$(yq eval '.database.instance' "${BASE_DIR}/../charts/${chart_name}/values-${cluster_name}.yaml")
-database=$(yq eval '.database.db' "${BASE_DIR}/../charts/${chart_name}/values.yaml")
-database_iam_user=$(yq eval '.database.user' "${BASE_DIR}/../charts/${chart_name}/values-${cluster_name}.yaml")
 migration_script_gcs_object_uri="gs://${ef_migrations_bucket}/${database}/${tag}_migration_script.sql"
 
 echo "Checking if entity framework migration script for tag: ${tag} and database: ${database} exists in the bucket."
