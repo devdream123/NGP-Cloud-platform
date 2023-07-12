2c8e5d06f36588b06a48af5c9ebf9ea13b8000dc
Tohid <91237829+tohid-Q@users.noreply.github.com>
2023-07-12 14:45:19 +1000

    chore:NSPS2-0000 remove deprecated /unused changes (#1427)
    * chore:NSPS2-0000 remove deprecated /unused changes
    
    * revert pre commit and remove back process

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,19 +14,6 @@ echo "using base dir: ${BASE_DIR}"
 
 for environment in "${environments[@]}"; do
 
-  if [ "${environment}" == "prd" ]; then
-
-    echo "Starting prod firestore backup"
-    gcloud scheduler jobs run ngp-prd-us1-firestore_backup_schedule1-csj --location=us-central1 --project=gcp-wow-corp-qretail-ngp-prod
-    echo "Waiting 60 seconds for firestore backup job"
-    sleep 60
-  
-  fi
-
-done
-
-for environment in "${environments[@]}"; do
-  
   "${BASE_DIR}"/install-helm-charts.sh "${environment}" 
   "${BASE_DIR}"/enable-service-mesh-features.sh "${environment}" 
 
