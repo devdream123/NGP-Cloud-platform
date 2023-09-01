16102b06f7fd8772be624465754f223a5f0b7d5e
Tohid <91237829+tohid-Q@users.noreply.github.com>
2023-09-01 17:21:24 +1000

    NSPS2-0000 Fix the cloud deployment issue. (#1646)

--- scripts/export-env-variables.sh
+++ scripts/export-env-variables.sh
@@ -20,7 +20,7 @@ environment=$1
 export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 CLOUDSDK_COMPUTE_REGION=$(yq eval '.env.CLOUDSDK_COMPUTE_REGION' "${BASE_DIR}/../config/${environment}.env.yaml")
 export CLOUDSDK_COMPUTE_REGION
-CLOUDSDK_CONTAINER_CLUSTERS=$(yq eval '.env.CLOUDSDK_CONTAINER_CLUSTERS[]' "${BASE_DIR}/../config/${environment}.env.yaml")
+readarray  CLOUDSDK_CONTAINER_CLUSTERS < <(yq eval '.env.CLOUDSDK_CONTAINER_CLUSTERS[]' "${BASE_DIR}/../config/${environment}.env.yaml")
 export CLOUDSDK_CONTAINER_CLUSTERS
 GCLOUD_PROJECT=$(yq eval '.env.GCLOUD_PROJECT' "${BASE_DIR}/../config/${environment}.env.yaml")
 export GCLOUD_PROJECT
--- scripts/package-store-charts.sh
+++ scripts/package-store-charts.sh
@@ -22,13 +22,13 @@ artifact_registry_uri=$1
 
 for dir in "${charts_dir}"/{.,}*; do
 
-  helm package "${charts_dir}/${dir}" --destination "${destination_dir}"
+  helm package "${dir}" --destination "${destination_dir}"
 
 done
 
 for package in "${destination_dir}"/{.,}*; do
 
-  echo "${destination_dir}/${package}"
-  helm push "${destination_dir}/${package}" oci://"${artifact_registry_uri}"
+  echo "${package}"
+  helm push "${package}" oci://"${artifact_registry_uri}"
 
 done
