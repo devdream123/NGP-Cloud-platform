fc576e4156d11e58c5521511235dac5190b05a50
Tohid <91237829+tohid-Q@users.noreply.github.com>
2023-09-15 17:35:47 +1000

    Fix/nsps2 9136 (#1706)

--- scripts/apply-entity-framework-migrations.sh
+++ scripts/apply-entity-framework-migrations.sh
@@ -14,6 +14,16 @@ function print_usage() {
   printf "Database IAM user is required.\n"
 }
 
+function sql_operations_wait() {
+  echo "Waiting for any existing database operations to finish on ${cloud_sql_instance} before running the migration script ..."
+  gcloud sql operations list \
+    --instance "${cloud_sql_instance}" \
+    --filter='NOT status:done' \
+    --format='value(name)' \
+    --project="${gcp_project_id}" \
+    | xargs -r gcloud sql operations wait --project="${gcp_project_id}"
+}
+
 if [ ! "$1" ] || [ ! "$2" ] || [ ! "$3" ] || [ ! "$4" ] || [ ! "$5" ] || [[ ! "$6" ]] || [[ ! "$7" ]]; then
   print_usage
   exit 1
@@ -22,7 +32,7 @@ fi
 ef_migrations_bucket=$1
 chart_name=$2
 cluster_name=$3
-gcp_project=$4
+gcp_project_id=$4
 cloud_sql_instance=$5
 database=$6
 database_iam_user=$7
@@ -33,12 +43,14 @@ migration_script_gcs_object_uri="gs://${ef_migrations_bucket}/${database}/${tag}
 echo "Checking if entity framework migration script for tag: ${tag} and database: ${database} exists in the bucket."
 
 if [[ $(gsutil ls "${migration_script_gcs_object_uri}") ]]; then
-  
+
+  sql_operations_wait
+
   echo "Importing entity framework migration script for tag: ${tag} into the database: ${database} on ${cloud_sql_instance} cloud SQL instance."
   gcloud sql import sql "${cloud_sql_instance}" "${migration_script_gcs_object_uri}" \
     --database="${database}" \
     --user="${database_iam_user}" \
-    --project="${gcp_project}"
+    --project="${gcp_project_id}"
 
 else
   
